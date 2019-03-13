d04829e8c435094c38d53f56c42156ba10a9da75
gmendes0 <gmendes230@gmail.com>
2019-03-13 11:09:31 -0300

    Conexão com o DB

---
+++ scripts/php/banco.php
@@ -0,0 +1,36 @@
+<?php
+
+    class Banco{
+
+        public static function conectar(){
+
+            $host = 'localhost'; // Host
+            $dbn = '';  // Nome do banco
+            $user = 'root'; // Usuario do DB
+            $senha = ''; // Senha do DB
+
+            try{
+
+                // Tentar conexão
+                $conn = new PDO("host=$host;dbname=$dbn", $user, $senha);
+
+            }catch(PDOException $erro){
+
+                // Exibir erro
+                die("Falha de conexão com o banco de dados: ".$erro->getMessage());
+
+            }
+
+            return $conn;
+
+        }
+
+        public static function desconectar(){
+
+            $conn = null;
+
+        }
+
+    }
+
+?>
