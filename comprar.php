0cbb9469f5c28c0edfc84809d359890d105af94c
gmendes0 <gmendes230@gmail.com>
2019-04-14 21:52:42 -0300

    Comprar Adicionado

--- comprar.php
+++ comprar.php
@@ -4,6 +4,9 @@
 
     if(!empty($_SESSION['venda'])){
 
+        include_once 'lib/DaoProduto.php';
+        $subtotal = null;
+
         if(!empty($_POST)){
 
             if(isset($_POST['qtd'])){
@@ -27,6 +30,7 @@
     echo '</pre>';
 
 ?>
+<!-- Exigir o Login -->
 <!DOCTYPE html>
 <html>
     <head>
@@ -38,5 +42,52 @@
 
     <body>
         
+        <div class="site">
+
+            <div class="pedido">
+
+                <h2>Seu Pedido</h2>
+
+                <ul><!-- Lista os produtos pedidos -->
+                    <!-- Header -->
+                    <li>
+                        <p>
+                            <span>Produto</span>
+                            <span>Total</span>
+                        </p>
+                    </li>
+
+                    <!-- produtos -->
+                    <?php
+                        foreach($_SESSION['venda'] as $prod => $qtd){
+                            $produto = DaoProduto::getInstance()->readOne($prod);
+                            $subtotal += $produto['valor']*$qtd;
+                    ?>
+                        <li>
+                            <span><?php echo $produto['nome']; ?></span><!-- Nome do Produto -->
+                            <span>x<?php echo $qtd; ?></span><!-- Quantidade de produtos no pedido -->
+                            <span>R$ <?php echo number_format($produto['valor']*$qtd, 2, ',', '.'); ?></span><!-- Valor total em relação a quantidade -->
+                        </li>
+                    <?php } ?>
+                </ul>
+
+                <ul><!-- Subtotal + Frete -->
+                    <li>
+                        <span>subtotal</span>
+                        <span>R$ <?php echo number_format($subtotal, 2, ',', '.'); ?></span>
+                    </li>
+                </ul>
+
+                <ul>
+                    <li>
+                        <span>Total</span>
+                        <span>R$ <?php echo number_format($subtotal, 2, ',', '.'); ?></span><!-- somar com o frete -->
+                    </li>
+                </ul>
+
+            </div>
+
+        </div>
+
     </body>
 </html>
