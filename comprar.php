201ed07191638fc855914ef8b3ad12fea75952ea
gmendes0 <gmendes230@gmail.com>
2019-04-15 00:19:03 -0300

    Compra

--- comprar.php
+++ comprar.php
@@ -37,57 +37,107 @@
         <meta charset="UTF-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet"/>
+        <link rel="stylesheet" href="css/bootstrap.css"/>
         <title>Comprar</title>
     </head>
 
     <body>
         
-        <div class="site">
+        <div class="container">
 
-            <div class="pedido">
+        <div class="col-lg-6 order-lg-2 mb-9 mb-lg-0">
+            
+            <div class="mb-4">
+                    
+                <h2 class="h2">Seu Pedido</h2>
 
-                <h2>Seu Pedido</h2>
+            </div>
 
-                <ul><!-- Lista os produtos pedidos -->
-                    <!-- Header -->
-                    <li>
-                        <p>
-                            <span>Produto</span>
-                            <span>Total</span>
-                        </p>
-                    </li>
+            <div class="card shadow-sm mb-5">
 
+                <div class="card-body p-5">
+                
                     <!-- produtos -->
                     <?php
                         foreach($_SESSION['venda'] as $prod => $qtd){
                             $produto = DaoProduto::getInstance()->readOne($prod);
                             $subtotal += $produto['valor']*$qtd;
                     ?>
-                        <li>
-                            <span><?php echo $produto['nome']; ?></span><!-- Nome do Produto -->
-                            <span>x<?php echo $qtd; ?></span><!-- Quantidade de produtos no pedido -->
-                            <span>R$ <?php echo number_format($produto['valor']*$qtd, 2, ',', '.'); ?></span><!-- Valor total em relação a quantidade -->
-                        </li>
+
+                    <div class="media align-items-center mb-5">
+
+                        <div class="media-body">
+                            <h2 class="h6 mb-0"><?php echo $produto['nome']; ?></h2>
+                            <small class="d-block text-secondary">x<?php echo $qtd; ?></small>
+                        </div>
+
+                        <div class="media-body text-right">
+                            <span>R$ <?php echo number_format($produto['valor']*$qtd, 2, ',', '.'); ?></span>
+                        </div>
+
+                    </div>
+
                     <?php } ?>
-                </ul>
+                    <!-- Fim produtos -->
 
-                <ul><!-- Subtotal + Frete -->
-                    <li>
-                        <span>subtotal</span>
-                        <span>R$ <?php echo number_format($subtotal, 2, ',', '.'); ?></span>
-                    </li>
-                </ul>
+                    <hr class="my-5"/>
 
-                <ul>
-                    <li>
-                        <span>Total</span>
-                        <span>R$ <?php echo number_format($subtotal, 2, ',', '.'); ?></span><!-- somar com o frete -->
-                    </li>
-                </ul>
+                    <!-- Subtotal + Frete -->
+                    <div class="media align-items-center">
+                        
+                        <h3 class="h6 text-secondary mr-3">Subtotal</h3>
+
+                        <div class="media-body text-right">
+
+                            <span>R$ <?php echo number_format($subtotal, 2, ',', '.'); ?></span>
+
+                        </div>
+
+                    </div>
+                    <!-- FIM Subtotal -->
+
+                    <!-- Frete -->
+                    <div class="media align-items-center">
+                        
+                        <h3 class="h6 text-secondary mr-3">Frete</h3>
+
+                        <div class="media-body text-right">
+
+                            <span>R$ 0,00</span>
+
+                        </div>
+
+                    </div>
+                    <!-- Fim Frete -->
+
+                    <hr class="my-5"/>
+
+                    <!-- Total -->
+                    <div class="media align-items-center">
+                        
+                        <h3 class="h6 text-secondary mr-3">Total</h3>
+
+                        <div class="media-body text-right">
+
+                            <span>R$ <?php echo number_format($subtotal, 2, ',', '.'); ?></span>
+
+                        </div>
+
+                    </div>
+                    <!-- Fim Total -->
+
+                </div>
 
             </div>
 
         </div>
+
+    </div>
+
+        <script src="js/jquery.js"></script>
+        <script src="js/popper.js"></script>
+        <script src="js/bootstrap.js"></script>
 
     </body>
 </html>
