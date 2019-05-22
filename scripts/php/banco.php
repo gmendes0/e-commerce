a0b1dab687e0eca7b5bffec12983d66fed4dc6ed
gmendes0 <gmendes230@gmail.com>
2019-05-21 23:02:45 -0300

    database config

--- scripts/php/banco.php
+++ scripts/php/banco.php
@@ -7,7 +7,7 @@
             $host = 'den1.mysql5.gear.host'; // Host
             $dbn = 'tcc3';  // Nome do banco
             $user = 'tcc3'; // Usuario do DB
-            $senha = '	Gm6yd3~2_vuW'; // Senha do DB
+            $senha = 'Gm6yd3~2_vuW'; // Senha do DB
 
             try{
 
