983a3baa26acc33c840fbd0d7014aa948bbe6293
gmendes0 <gmendes230@gmail.com>
2019-06-12 14:56:05 -0300

    curlopt post true

--- lib/pagseguroct/pagamento.php
+++ lib/pagseguroct/pagamento.php
@@ -9,7 +9,7 @@
      */
     $curl = curl_init($url);
     curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/xml; charset=UTF-8'));
-    curl_setopt($curl, CURLOPT_POST, 1);
+    curl_setopt($curl, CURLOPT_POST, true);
     curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
     $retorno = curl_exec($curl);
